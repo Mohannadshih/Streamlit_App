1aed668283f3646578791a2920ba470fb314652c
Mohannad Shihadeh <96916333+Mohannadshih@users.noreply.github.com>
2023-10-25 08:22:50 -0400

    added extra dataframe for test.csv

--- main.py
+++ main.py
@@ -44,3 +44,22 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
 
+#different data
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv("test_data/test1.csv", nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data', key=2):
+    st.subheader('Raw data')
+    st.write(data)
